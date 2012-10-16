f55719ada4947eddf38e696796e5c3538e52ef8f
issyl0 <isabell@issyl0.co.uk>
2012-10-16 01:14:46 +0100

    Add loved/recent track viewing + overall track number.
    Display_banned_tracks() is not funcional.  Also needs some code cleanup.

--- lastfm.rb
+++ lastfm.rb
@@ -1,29 +1,66 @@
 # Isabell Long <isabell@issyl0.co.uk>, 2012.
-# Look at someone's last.fm without opening a web browser.
+# Look at someone's last.fm profile without opening a web browser.
 require 'rubygems'
 require 'rest_client'
 require 'json'
 
+def align_columns(trackname,trackartist)
+  column_width = 70
+  spaces = ' ' * (column_width - trackname.length)
+  puts trackname + spaces + trackartist
+end
+
 def display_recently_played(entire_parsed_json)
+  # Display recent tracks for the specified user.
+  entire_parsed_json["recenttracks"]["track"].each do |track|
+    
+    trackname = track["name"].to_s
+    trackartist = track["artist"]["#text"].to_s
+
+    align_columns(trackname,trackartist)
+  end
 end
 
 def display_num_tracks(entire_parsed_json)
+  # Capitalise the username for aesthetic value.  Print it and the
+  # user's play count.
+  puts entire_parsed_json["user"]["name"].to_s.capitalize + "'s play count:\t" + entire_parsed_json["user"]["playcount"].to_s
 end
 
 def display_loved_tracks(entire_parsed_json)
+  # The specified user's loved tracks.
+  entire_parsed_json["lovedtracks"]["track"].each do |track|
+    
+    trackname = track["name"].to_s
+    trackartist = track["artist"]["name"].to_s
+    
+    align_columns(trackname,trackartist) 
+  end
 end
 
 def display_banned_tracks(entire_parsed_json)
+  # Doesn't currently work due to trackartist.
+  puts entire_parsed_json
+  entire_parsed_json["bannedtracks"]["track"].each do |track|
+
+    trackname = track[1].to_s
+    trackartist = track["artist"]["name"].to_s
+
+    align_columns(trackname,trackartist)
+  end
 end
 
 def find_results(username,apikey,method,limit)
-  url = "http://ws.audioscrobbler.com/2.0/?method=#{method}#{limit}&user=#{username}&api_key=#{apikey}&format=json"
+  url = "http://ws.audioscrobbler.com/2.0/?method=#{method}&user=#{username}&api_key=#{apikey}&format=json"
+  if method != "user.getinfo" then
+    url = "http://ws.audioscrobbler.com/2.0/?method=#{method}#{limit}&user=#{username}&api_key=#{apikey}&format=json"
+  end
   response = RestClient.get(url)
   entire_parsed_json = JSON.parse(response)
   
   if method == "user.getrecenttracks"
     display_recently_played(entire_parsed_json)
-  elsif method == "user.getuserinfo"
+  elsif method == "user.getinfo"
     display_num_tracks(entire_parsed_json)
   elsif method == "user.getlovedtracks"
     display_loved_tracks(entire_parsed_json)
@@ -37,8 +74,9 @@ end
 puts "Enter your API key:"
 apikey = gets.chomp
 
-puts "Is opening a web browser too much effort?  Enter a username to research:"
+puts "Enter a username to find out about:"
 username = gets.chomp
+
 puts "What would you like to find out about #{username}'s music?\n
   1. Recently played tracks.\n
   2. Number of tracks listened to.\n
@@ -59,7 +97,7 @@ if choice == 1 then
   method = "user.getrecenttracks"
   find_results(username,apikey,method,limit)
 elsif choice == 2 then
-  method = "user.getuserinfo"
+  method = "user.getinfo"
   find_results(username,apikey,method,limit)
 elsif choice == 3 then
   method = "user.getlovedtracks"
