98512ad1c80776c376c5977c2f2d51c27891ca98
issyl0 <isabell@issyl0.co.uk>
2012-10-24 18:22:19 +0100

    Add validation. Standardise if syntax across the file.

--- lastfm.rb
+++ lastfm.rb
@@ -12,39 +12,59 @@ end
 
 def display_recently_played(entire_parsed_json)
   # Display recent tracks for the specified user.
-  entire_parsed_json["recenttracks"]["track"].each do |track|
+  if entire_parsed_json["recenttracks"]["track"] == nil then
+    puts "No recent tracks.\n"
+  else
+    entire_parsed_json["recenttracks"]["track"].each do |track|
     
-    trackname = track["name"].to_s
-    trackartist = track["artist"]["#text"].to_s
+      trackname = track["name"].to_s
+      trackartist = track["artist"]["#text"].to_s
     
-    align_columns(trackname,trackartist) 
+      align_columns(trackname,trackartist) 
+    end
   end
 end
 
 def display_num_tracks(entire_parsed_json)
   # Capitalise the username for aesthetic value.  Print it and the
   # user's play count.
-  puts entire_parsed_json["user"]["name"].to_s.capitalize + "'s play count:\t" + entire_parsed_json["user"]["playcount"].to_s
+  
+  if entire_parsed_json["user"]["playcount"] == nil then
+    puts "No tracks played.\n"
+  else
+    puts entire_parsed_json["user"]["name"].to_s.capitalize + "'s play count:\t" + entire_parsed_json["user"]["playcount"].to_s
+  end
 end
 
 def display_loved_tracks(entire_parsed_json)
   # The specified user's loved tracks.
-  entire_parsed_json["lovedtracks"]["track"].each do |track|
+  
+  if entire_parsed_json["lovedtracks"]["track"] == nil then
+    puts "No loved tracks.\n"
+  else
+    entire_parsed_json["lovedtracks"]["track"].each do |track|
     
-    trackname = track["name"].to_s
-    trackartist = track["artist"]["name"].to_s
+      trackname = track["name"].to_s
+      trackartist = track["artist"]["name"].to_s
     
-    align_columns(trackname,trackartist) 
+      align_columns(trackname,trackartist) 
+    end
   end
 end
 
 def display_banned_tracks(entire_parsed_json)
-  entire_parsed_json["bannedtracks"]["track"].each do |track|
+  # Specified user's banned tracks (currently only > 1).
+  
+  if entire_parsed_json["bannedtracks"]["track"] == nil then
+    puts "No banned tracks.\n"
+  else
+    entire_parsed_json["bannedtracks"]["track"].each do |track|
     
-    trackname = track["name"].to_s
-    trackartist = track["artist"]["name"].to_s
+      trackname = track["name"].to_s
+      trackartist = track["artist"]["name"].to_s
     
-    align_columns(trackname,trackartist)
+      align_columns(trackname,trackartist)
+    end
   end
 end
 
@@ -56,13 +76,13 @@ def find_results(username,apikey,method,limit)
   response = RestClient.get(url)
   entire_parsed_json = JSON.parse(response)
   
-  if method == "user.getrecenttracks"
+  if method == "user.getrecenttracks" then
     display_recently_played(entire_parsed_json)
-  elsif method == "user.getinfo"
+  elsif method == "user.getinfo" then
     display_num_tracks(entire_parsed_json)
-  elsif method == "user.getlovedtracks"
+  elsif method == "user.getlovedtracks" then
     display_loved_tracks(entire_parsed_json)
-  elsif method == "user.getbannedtracks"
+  elsif method == "user.getbannedtracks" then
     display_banned_tracks(entire_parsed_json)
   end
 end
